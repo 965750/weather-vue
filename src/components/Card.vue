bd80263a6c303f9bad871d960b60e9ab91d5efdd
AdrianZ <aventhelast@gmail.com>
2019-09-25 15:40:36 +0200

    project initialized

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+
+    </div>
+</template>
+
+<style lang="scss">
+    .card {
+        width: 300px;
+        height: 500px;
+        border: 1px solid red;
+        margin: auto;
+        border-radius: 10px;
+    }
+</style>
